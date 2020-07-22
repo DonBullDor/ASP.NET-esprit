9624aeab9ce5e193c8e99e9955d0e5d6efe9d055
Mohamed Ben Abbes <mohamedbenabbes6@gmail.com>
2020-07-22 09:11:24 +0200

    Add files via upload

---
+++ MyFinance/Solution.Data/Infrastructures/DataBaseFactory.cs
@@ -0,0 +1,27 @@
+﻿namespace Solution.Data.Infrastructure
+{
+    public class DataBaseFactory : Disposable,IDataBaseFactory
+    {
+        private MyContext dataContext;
+
+        public MyContext DataContext 
+        {
+            get { return dataContext; }
+            
+        }
+
+        public DataBaseFactory()
+        {
+            dataContext = new MyContext();
+        }
+        protected override void DisposeCore() //libérer l'espace mémoire occupé par le ctx
+        {
+            if(DataContext!=null)
+
+           DataContext.Dispose();
+            
+        }
+
+
+    }
+}
---
+++ MyFinance/Solution.Data/MyContext.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.Entity;
+
+namespace Solution.Data
+{
+    public class MyContext:DbContext
+    {
+        public MyContext():base("name=MaChaine")
+        {
+
+        }
+        //les DbSet<> ....
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            /*modelBuilder.Configurations.Add(.....);
+            modelBuilder.Conventions.Add(.....);*/
+
+        }
+    }
+}
